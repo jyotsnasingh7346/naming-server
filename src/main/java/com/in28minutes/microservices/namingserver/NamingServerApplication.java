3a5bcf5487359402ed6c15c334f57b8744c1ce67
Jyotsna <jyotsnasingh7346@gmail.com>
2024-02-27 01:37:07 +0530

    namingserver

--- src/main/java/com/in28minutes/microservices/namingserver/NamingServerApplication.java
+++ src/main/java/com/in28minutes/microservices/namingserver/NamingServerApplication.java
@@ -17,9 +17,9 @@ public class NamingServerApplication {
 	 * TO CREATE A NAMING SERVER, ONE OF THE OPTIONS THAT THE SPRING CLOUD PROVIDES IS EUREKA.
 	 * 
 	 * Step 01 : On start.spring.io, 
-	 * Create a project (NAMING SERVER) with the dependencies Devtools, Actuator, & 
-	 * Eureka Server (spring-cloud-netflix Eureka Server). Generate & import in your workspace.
-	 * (Make sure to put it on git)
+	 * 			Create a project (NAMING SERVER) with the dependencies Devtools, Actuator, & 
+	 * 			Eureka Server (spring-cloud-netflix Eureka Server). Generate & import in your workspace.
+	 * 			(Make sure to put it on git)
 	 * 
 	 * Step 02 : @EnableEurekaServer on the naming-server.SpringBootApplication Launcher class.
 	 * 
@@ -30,7 +30,7 @@ public class NamingServerApplication {
 	 */
 	// *************************************************************************************************************
 	/*
-	 * STEPS TO CONNECT REGISTER THE CURRENCY CONVERSION & EXCHANGE MICROSERVICE WITH THIS NAMING SREVER :  
+	 * STEPS TO CONNECT / REGISTER THE CURRENCY CONVERSION & EXCHANGE MICROSERVICE WITH THIS NAMING SREVER :  
 	 * 
 	 * Step 01 : In the pom files of all the microservices that need to be registered with the naming server, 
 	 * add the dependency, after the spring-cloud-starter-config dependency :
@@ -39,11 +39,11 @@ public class NamingServerApplication {
 			<artifactId>spring-cloud-starter-netflix-eureka-client</artifactId>
 		</dependency>
 	 * 
-	 * Step 02 : Launch the Naming Server (this application) & the registered application, and 
-	 * you should find the registered apps on the eureka server.
-	 * 
-	 * Step 03 : In the application.properties of all the registered applications, add their eureka client :
+	 * Step 02 : In the application.properties of all the registered applications, add their eureka client :
 	 * 		> eureka.client.serviceUrl.defaultZone=http://localhost:8761/eureka
+	 * 
+	 * Step 03 : Launch the Naming Server (this application) & the registered application, and 
+	 * you should find the registered apps on the eureka server.
 	 * 
 	 * Step 04 : You will find the registered applications & their URLs on the eureka server.
 	 * > You can also find the UP & DOWN status in the logs.
